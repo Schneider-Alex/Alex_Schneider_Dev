fd14efb100aeb90e9a4fd89f9a2ab08f7720d18f
Schneider-Alex <alexmanuelschneider@gmail.com>
2022-06-07 16:14:08 -0400

    Sidebar CSS completed, moving to other Components
    Ready to design main/resume pages

--- dev_page/src/SidebarOption.js
+++ dev_page/src/SidebarOption.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import "./SidebarOption.css"
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
 
 function HeaderOption({Icon, title,link}) {
   return (
     <div className="sidebarOption">
-      
-      <a className="headerOption_title" href={'/' + link}>
-        {Icon && <Icon className="headerOption_icon"/>}
-        <h3>{title}</h3>
+      <a className="sidebarOption_title" href={'/' + link}>
+        {Icon && <Icon className="sidebarOption_icon"/>}
+        <h3><ArrowRightIcon/>{title}</h3>
       </a>
     </div>
   )
