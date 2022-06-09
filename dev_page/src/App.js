9aa0bc96ba0c1b7c23ed5e7d50110096ee4741cc
Schneider-Alex <alexmanuelschneider@gmail.com>
2022-06-09 16:59:35 -0400

    Styling animations for css buttons on hover
    css changes, very happy about these

--- dev_page/src/App.js
+++ dev_page/src/App.js
@@ -13,6 +13,7 @@ function App() {
   return (
     <div className="app">
       <div className="main">
+        <Header Icon={ChevronLeftIcon}/>
         <BrowserRouter>
           <Routes>
             <Route path="index" element={<Main/>}/>
