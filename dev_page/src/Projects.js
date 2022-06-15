c70a6c686a4df3f5852f48520773a075c2f32a4a
Schneider-Alex <alexmanuelschneider@gmail.com>
2022-06-15 17:27:20 -0400

    figured out spacing/sizing for Project Options

--- dev_page/src/Projects.js
+++ dev_page/src/Projects.js
@@ -13,6 +13,7 @@ function Projects() {
             <Header Icon={AssignmentIcon} />
             <div className="projects_options">
                 <ProjectOption title="LinkedIn Clone" pic="/linkedinpic1.png" vid="/LinkedInClone.mov" description="A front-end focused recreation of the style of LinkedIn, using React and basic CSS on the front end. The application requires email verification driven by Firebase for account creation. Once an account is created users can add a post to a real time Firebase database which will then display the post in the center feed which is sorted by a timestamp." />
+                <ProjectOption title="OnTrack" vid="/OnTrackVideo.mov"/>
             </div>
         </div>
     )
