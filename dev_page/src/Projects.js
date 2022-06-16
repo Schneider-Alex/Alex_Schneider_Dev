5d2fde26a2bf81cc0839471997565d3343a1272e
Schneider-Alex <alexmanuelschneider@gmail.com>
2022-06-16 09:29:55 -0400

    fixed Headers

--- dev_page/src/Projects.js
+++ dev_page/src/Projects.js
@@ -10,7 +10,7 @@ function Projects() {
     return (
         
         <div className="projects">
-            <Header Icon={AssignmentIcon} />
+            <Header header="My Projects" />
             <div className="projects_options">
                 <ProjectOption title="LinkedIn Clone" pic="/linkedinpic1.png" vid="/LinkedInClone.mov" description="A front-end focused recreation of the style of LinkedIn, using React and basic CSS on the front end. The application requires email verification driven by Firebase for account creation. Once an account is created users can add a post to a real time Firebase database which will then display the post in the center feed which is sorted by a timestamp." />
                 <ProjectOption title="OnTrack" vid="/OnTrackVideo.mov"/>
