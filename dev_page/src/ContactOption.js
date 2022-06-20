a62d9053840e465ad49387733feff69ccd1c3cb9
Schneider-Alex <alexmanuelschneider@gmail.com>
2022-06-20 10:51:50 -0400

    changed third contact option to resume downlaod

--- dev_page/src/ContactOption.js
+++ dev_page/src/ContactOption.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import "./ContactOption.css"
 
-function ContactOption({ Icon, link, target,name }) {
+function ContactOption({ Icon, link, target,name,download }) {
     return (
         <div className="contactOption_tert">
             <div className="contactOption_wrapper">
                 <div className="contactOption">
-                    {Icon && <a href={link} target={target}><Icon className="contactOption_icon" /></a>}
+                    {Icon && <a href={link} target={target} download={download}><Icon className="contactOption_icon" /></a>}
                 </div>
             </div>
             <p>{name}</p>
--- dev_page/src/Contacts.js
+++ dev_page/src/Contacts.js
@@ -5,8 +5,8 @@ import "./Contacts.css"
 import ContactOption from "./ContactOption.js"
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
-import EmailIcon from '@mui/icons-material/Email';
 import Sidebar from './Sidebar.js';
+import FilePresentRoundedIcon from '@mui/icons-material/FilePresentRounded';
 
 function Contacts() {
     return (
@@ -17,9 +17,9 @@ function Contacts() {
                     <Sidebar/>
                 </div>
                 <div className="contacts_content">
-                    <ContactOption Icon={LinkedInIcon} link="https://www.linkedin.com/in/alex-schneider-stay-learning/" target="_blank" name="LinkedIn" />
-                    <ContactOption Icon={GitHubIcon} link="https://github.com/Schneider-Alex" target="_blank" name="GitHub" />
-                    <ContactOption Icon={EmailIcon} link="/message" target="" name="Message Me" />
+                    <ContactOption Icon={LinkedInIcon} link="https://www.linkedin.com/in/alex-schneider-stay-learning/" target="_blank" name="LinkedIn" download=""/>
+                    <ContactOption Icon={GitHubIcon} link="https://github.com/Schneider-Alex" target="_blank" name="GitHub" download="" />
+                    <ContactOption Icon={FilePresentRoundedIcon} link="Alexander Schneider - Resume 1.1.pdf" target="" name="Download Resume" download="Alex Schneider Resume"/>
                 </div>
             </div>
         </div>
