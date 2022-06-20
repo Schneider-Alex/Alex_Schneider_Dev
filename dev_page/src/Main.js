d98d33dd8c5bc1bdaaab31578a1b2baaa0f0d4a1
Schneider-Alex <alexmanuelschneider@gmail.com>
2022-06-20 11:31:05 -0400

    Home Page Finished

--- dev_page/src/Main.js
+++ dev_page/src/Main.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import "./Main.css"
 import Header from "./Header.js"
+import Sidebar from "./Sidebar.js";
+import SidebarOption from "./SidebarOption.js";
 import CottageIcon from '@mui/icons-material/Cottage';
-import Sidebar from "./Sidebar.js"
+import AssignmentIcon from '@mui/icons-material/Assignment';
+import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
 
 
 function Main() {
@@ -14,6 +17,18 @@ function Main() {
             <Sidebar/>
           </div>
           <div className="main_content">
+            <div className="main_content_top">
+            <h1>Alex Schneider</h1>
+            <h3>-Chemical Engineer Turned Web Developer-</h3>
+            </div>
+            <div className="main_content_bottom">
+            <h3>Welcome to  my site! 
+              Please feel free to explore:</h3>
+              <div className="main_content_bottom_links">
+                <SidebarOption title="Projects" link="projects" Icon={AssignmentIcon}/>
+                <SidebarOption title="Contact" link="contacts" Icon={AlternateEmailIcon}/>
+                </div>
+            </div>
           </div>
         </div>
         
